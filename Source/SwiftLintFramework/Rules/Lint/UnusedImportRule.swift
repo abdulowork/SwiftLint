0032b5bfb6f7d6555fa37a960bbd42d33c88ecb3
Timofey Solonin <tim-solonin2@yandex.ru>
2019-08-19 18:51:19 +0300

    #2737 - Fix false positive violation for the module where only operators are invoked

--- Source/SwiftLintFramework/Rules/Lint/UnusedImportRule.swift
+++ Source/SwiftLintFramework/Rules/Lint/UnusedImportRule.swift
@@ -28,6 +28,12 @@ public struct UnusedImportRule: CorrectableRule, ConfigurationProviderRule, Anal
             """
             import UnknownModule
             func foo(error: Swift.Error) {}
+            """,
+            """
+            import Foundation
+            import ObjectiveC
+            let 👨‍👩‍👧‍👦 = #selector(NSArray.contains(_:))
+            👨‍👩‍👧‍👦 == 👨‍👩‍👧‍👦
             """
         ],
         triggeringExamples: [
@@ -161,64 +167,97 @@ public struct UnusedImportRule: CorrectableRule, ConfigurationProviderRule, Anal
 
 private extension File {
     func unusedImports(compilerArguments: [String]) -> [(String, NSRange)] {
-        let contentsNSString = contents.bridge()
-        var imports = Set<String>()
-        var usrFragments = Set<String>()
-        var nextIsModuleImport = false
-        for token in syntaxMap.tokens {
-            guard let tokenKind = SyntaxKind(rawValue: token.type) else {
-                continue
-            }
-            if tokenKind == .keyword,
-                let substring = contentsNSString.substringWithByteRange(start: token.offset, length: token.length),
-                substring == "import" {
-                nextIsModuleImport = true
-                continue
-            }
-            if syntaxKindsToSkip.contains(tokenKind) {
-                continue
-            }
-            let cursorInfoRequest = Request.cursorInfo(file: path!, offset: Int64(token.offset),
-                                                       arguments: compilerArguments)
-            guard let cursorInfo = try? cursorInfoRequest.sendIfNotDisabled() else {
-                queuedPrintError("Could not get cursor info")
-                continue
-            }
-            if nextIsModuleImport {
-                if let importedModule = cursorInfo["key.modulename"] as? String,
-                    cursorInfo["key.kind"] as? String == "source.lang.swift.ref.module" {
-                    imports.insert(importedModule)
-                    nextIsModuleImport = false
-                    continue
+        guard let index = try? Request.index(file: path!, arguments: compilerArguments).sendIfNotDisabled() else {
+            queuedPrintError("Could not get index")
+            return []
+        }
+
+        let flatEntities = self.flatEntities(entity: index)
+        let importedModules: Set<String> = Set(
+            flatEntities.compactMap {
+                guard let kind = $0["key.kind"] as? String else { return nil }
+                if kind == moduleKind {
+                    return $0["key.name"] as? String
                 } else {
-                    nextIsModuleImport = false
+                    return nil
                 }
             }
+        )
 
-            if let usr = cursorInfo["key.modulename"] as? String {
-                usrFragments.formUnion(usr.split(separator: ".").map(String.init))
-            }
-        }
-        // Always disallow 'import Swift' because it's available without importing.
-        usrFragments.remove("Swift")
-        var unusedImports = imports.subtracting(usrFragments)
-        // Certain Swift attributes requires importing Foundation.
-        if unusedImports.contains("Foundation") && containsAttributesRequiringFoundation() {
-            unusedImports.remove("Foundation")
-        }
+        let offsetPerLine = self.offsetPerLine()
+        var usedModules: Set<String> = Set(
+            flatEntities
+                .filter { entity in
+                    guard let kind = entity["key.kind"] as? String else { return false }
+                    if let syntaxKind = SyntaxKind(rawValue: kind),
+                        syntaxKindsToSkip.contains(syntaxKind) { return false }
+                    return kind != moduleKind
+                }
+                .compactMap { entity -> [String]? in
+                    guard let offset = entity.offset(in: offsetPerLine) else {
+                        queuedPrintError("Could not get offset")
+                        return nil
+                    }
+                    guard let cursorInfo = try? Request
+                        .cursorInfo(file: path!, offset: offset, arguments: compilerArguments)
+                        .sendIfNotDisabled() else {
+                            queuedPrintError("Could not get cursor info")
+                            return nil
+                    }
+                    return (cursorInfo["key.modulename"] as? String)?.split(separator: ".").map(String.init)
+                }
+                .flatMap { $0 }
+        )
+
+        if containsAttributesRequiringFoundation() { usedModules.insert("Foundation") }
+
+        return rangedAndSortedUnusedImports(
+            of: Array(importedModules.subtracting(usedModules)),
+            contents: contents.bridge()
+        )
+    }
+
+    func rangedAndSortedUnusedImports(of unusedImports: [String], contents: NSString) -> [(String, NSRange)] {
         return unusedImports
             .map { module in
-                let testableImportRange = contentsNSString.range(of: "@testable import \(module)\n")
+                let testableImportRange = contents.range(of: "@testable import \(module)\n")
                 if testableImportRange.location != NSNotFound {
                     return (module, testableImportRange)
                 }
 
-                return (module, contentsNSString.range(of: "import \(module)\n"))
+                return (module, contents.range(of: "import \(module)\n"))
             }
             .sorted(by: { $0.1.location < $1.1.location })
     }
 
-    private func containsAttributesRequiringFoundation() -> Bool {
+    typealias Entity = [String: SourceKitRepresentable]
+    func flatEntities(entity: Entity) -> [Entity] {
+        let entities = entity.entities
+        if entities.isEmpty {
+            return [entity]
+        } else {
+            return [entity] + entities.flatMap { flatEntities(entity: $0) }
+        }
+    }
+
+    func offsetPerLine() -> [Int: Int64] {
+        return [Int: Int64](
+            uniqueKeysWithValues: contents.bridge()
+                .components(separatedBy: "\n")
+                .map {
+                    Int64($0.bridge().lengthOfBytes(using: .utf8))
+                }
+                .reduce([Int64(0)]) { result, length in
+                    let newLineCharacterLength = Int64(1)
+                    let lineLength = length + newLineCharacterLength
+                    return result + [(result.last ?? 0) + lineLength]
+                }
+                .enumerated()
+                .map { ($0.offset, $0.element) }
+        )
+    }
+
+    func containsAttributesRequiringFoundation() -> Bool {
         guard contents.contains("@objc") else {
             return false
         }
@@ -234,6 +273,19 @@ private extension File {
         return containsAttributesRequiringFoundation(dict: self.structure.dictionary)
     }
 }
+
+private extension Dictionary where Key == String, Value == SourceKitRepresentable {
+    func offset(in offsetPerLine: [Int: Int64]) -> Int64? {
+        guard
+            let line = self["key.line"] as? Int64,
+            let column = self["key.column"] as? Int64,
+            let lineOffset = offsetPerLine[Int(line) - 1]
+            else { return nil }
+        return lineOffset + column - 1
+    }
+}
+
+private let moduleKind = "source.lang.swift.ref.module"
 
 private let syntaxKindsToSkip: Set<SyntaxKind> = [
     .attributeBuiltin,
